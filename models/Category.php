12ca951bed546e59bc5fb19242d4d1ce81ceaab2
HabibAroua <habibha.aroua82@gmail.com>
2020-10-07 21:43:56 +0100

    new class in models

---
+++ models/Category.php
@@ -0,0 +1,30 @@
+<?php
+    class Category
+    {
+        private $id;
+        private $name;
+        
+        // id
+        public function getId()
+        {
+            return $this->id;
+        }
+        
+        public function setId($id)
+        {
+            $this->id = $id;
+        }
+        // name
+        public function getName()
+        {
+            return $this->name;
+        }
+        
+        public function setName($name)
+        {
+            $this->name = $name;
+        }
+    }
+
+
+?>
