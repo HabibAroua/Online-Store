be5d6803f705adfcfda2939202015eb750e8b28c
HabibAroua <habibha.aroua82@gmail.com>
2020-10-08 23:19:20 +0100

    add new folder

---
+++ Admin/Test.php
@@ -0,0 +1,5 @@
+<?php
+    require ('../app/database/Connection.php');
+    require ('../app/models/Category.php');
+    require_once('../app/models/Test.php');
+?>
--- Test.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require 'app/database/Connection.php';
-    require 'app/models/Category.php';
-    require_once('app/models/Test.php');
-?>
