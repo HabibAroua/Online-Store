528aa79482208887cab84d7b8be449b585c516f0
HabibAroua <habibha.aroua82@gmail.com>
2020-10-16 21:16:42 +0100

    add getter and setter

---
+++ app/models/Product.php
@@ -0,0 +1,47 @@
+<?php
+    class Product
+    {
+        private $reference;
+        private $label;
+        private $price;
+        private $amount;
+        private $picture;
+        private $description;
+        
+        //reference
+        public function getReference()
+        {
+            return $this->reference;
+        }
+        
+        public function setReference($reference)
+        {
+            $this->reference = $reference;
+        }
+        
+        //label
+        public function getLabel()
+        {
+            return $this->label;
+        }
+        
+        public function setLabel($label)
+        {
+            $this->label = $label;
+        }
+        
+        //price
+        public function getPrice()
+        {
+            return $this->price;
+        }
+        
+        public function setPrice($price)
+        {
+            $this->price = $price;
+        }
+        //amount
+        //picture
+        //description
+    }
+?>
