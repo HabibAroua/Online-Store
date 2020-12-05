4bf81f40483c3da204988937e49c45109a359abd
HabibAroua <habibha.aroua82@gmail.com>
2020-12-06 00:53:22 +0100

    add comment

--- app/controllers/actions.php
+++ app/controllers/actions.php
@@ -1,5 +1,5 @@
 <?php
-
+    //https://speckyboy.com/css-js-notification-alert-code/
     //require_once('../database/Connection.php');
     require_once('../models/Category.php');
     require_once('../models/Product.php');
