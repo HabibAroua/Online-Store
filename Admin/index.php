fee45440c7dc785b7bc71884cb1aa9779298db55
HabibAroua <habibha.aroua82@gmail.com>
2020-12-21 22:28:46 +0100

    add form of register

--- Admin/index.php
+++ Admin/index.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Admin</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="javascript/Category.js"></script>
-    </head>
-    <body>
-        <script>
-            const c = new Category(1,'the name','the description');
-            c.insert();
-            c.action();
-        </script>
-</head>
-<body>
-
-<p>If you click on the "Hide" button, I will disappear.</p>
-
-<button id="hide">Hide</button>
-<button id="show">Show</button>
-        
-    </body>
+<html lang="en">
+	<?php
+		require_once('../app/views/Admin/header.php');
+	?>
+	<body>
+		<?php
+            if(isset($_GET['page']))
+            {
+                switch($_GET['page'])
+                {
+                    case 'login' : require_once('../app/views/Admin/login.php');
+                    break;
+                    case 'register' : require_once('../app/views/Admin/register.php');
+                    break;
+                }
+            }
+            else
+            {
+                
+            }
+        ?>
+	</body>
 </html>
---
+++ app/views/Admin/register.php
@@ -0,0 +1,86 @@
+<title>Register</title>
+<div class="limiter">
+	<div class="container-login100">
+		<div class="wrap-login100 p-t-90 p-b-30">
+			<form class="login100-form validate-form">
+				<span class="login100-form-title p-b-40">
+					Register
+				</span>	
+				<div>
+					<a href="#" class="btn-login-with bg1 m-b-10">
+						<i class="fa fa-facebook-official"></i>
+							Sign up with Facebook
+					</a>
+					<!--
+						<a href="#" class="btn-login-with bg2">
+                            <i class="fa fa-twitter"></i>
+							Login with Twitter
+						</a> -->
+				</div>
+	
+				<div class="text-center p-t-55 p-b-30">
+                    <span class="txt1">
+						Sign up
+					</span>
+				</div>
+                
+                <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your first name">
+					<input class="input100" type="text" name="first_name" placeholder="First name">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your last name">
+					<input class="input100" type="text" name="last_name" placeholder="Last name">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your login">
+					<input class="input100" type="text" name="login" placeholder="Login">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your birth date">
+					<input class="input100" type="date" name="birth_date" placeholder="Birth date">
+					<span class="focus-input100"></span>
+				</div>
+				<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter email: ex@abc.xyz">
+					<input class="input100" type="text" name="email" placeholder="Email">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your telephone number">
+					<input class="input100" type="telephone" name="telephone" placeholder="Telephone">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter your address">
+					<input class="input100" type="text" name="address" placeholder="Address">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter you first name">
+					<input class="input100" type="text" name="first_name" placeholder="First name">
+                    <select class="input100" placeholder="Country">
+                        <option>Select a country ...</option>
+                        <option>Japan</option>
+                        <option>Tunisia</option>
+                    </select>
+					<span class="focus-input100"></span>
+				</div>
+				<div class="wrap-input100 validate-input m-b-20" data-validate = "Please enter password">
+					<span class="btn-show-pass">
+						<i class="fa fa fa-eye"></i>
+					</span>
+					<input class="input100" type="password" name="password" placeholder="Password">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input m-b-20" data-validate = "Please confirm your password">
+					<span class="btn-show-pass">
+						<i class="fa fa fa-eye"></i>
+					</span>
+					<input class="input100" type="confirm_password" name="confirm_password" placeholder="Confirm Password">
+					<span class="focus-input100"></span>
+				</div>
+				<div class="container-login100-form-btn">
+					<button class="login100-form-btn">
+						Login
+                    </button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
