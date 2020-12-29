143b8d6650ab16db51f02977b80e69bf8200c00d
HabibAroua <habibha.aroua82@gmail.com>
2020-12-29 23:54:24 +0100

    write getters and setters in Product.js

--- Admin/js/Product.js
+++ Admin/js/Product.js
@@ -1,21 +1,83 @@
-/*
- private $reference;
-        private $label;
-        private $price;
-        private $amount;
-        private $picture;
-        private $description;
-        private $idCat;
- **/
 class Product
 {
-    constructor(label, price, amount, picture, description, idCat)
+    constructor(reference, label, price, amount, picture, description, idCat)
     {
+        this.reference = reference;
         this.label = label;
         this.price = price;
         this.amount = amount;
         this.pricture = picture;
         this.description = description;
         this.idCat = idCat;
+    }
+    
+    getReference()
+    {
+        return this.reference;
+    }
+    
+    setReference(reference)
+    {
+        this.reference = reference;
+    }
+    
+    getLabel()
+    {
+        return this.label;
+    }
+    
+    setLabel(label)
+    {
+        this.label = label;
+    }
+    
+    getPrice()
+    {
+        return this.price;
+    }
+    
+    setPrice(price)
+    {
+        this.price = price;
+    }
+    
+    getAmount()
+    {
+        return this.amount;
+    }
+    
+    setAmount(amount)
+    {
+        this.amount = amount;
+    }
+    
+    getPicture()
+    {
+        return this.picture;
+    }
+    
+    setPicture(picture)
+    {
+        this.picture = picture;
+    }
+    
+    getDescription()
+    {
+        return this.description;
+    }
+    
+    setDescription(description)
+    {
+        this.description = description;
+    }
+    
+    getDdCat()
+    {
+        return this.idCat;
+    }
+    
+    setIdCat(idCat)
+    {
+        return this.idCat;
     }
 }
