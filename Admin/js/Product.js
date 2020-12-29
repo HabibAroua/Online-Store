225939822d774e42e22cc61b51fda9fcbaa7bcc7
HabibAroua <habibha.aroua82@gmail.com>
2020-12-29 23:48:14 +0100

    add js file

---
+++ Admin/js/Product.js
@@ -0,0 +1,21 @@
+/*
+ private $reference;
+        private $label;
+        private $price;
+        private $amount;
+        private $picture;
+        private $description;
+        private $idCat;
+ **/
+class Product
+{
+    constructor(label, price, amount, picture, description, idCat)
+    {
+        this.label = label;
+        this.price = price;
+        this.amount = amount;
+        this.pricture = picture;
+        this.description = description;
+        this.idCat = idCat;
+    }
+}
